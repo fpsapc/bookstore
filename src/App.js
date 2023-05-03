f6e8ef714a34242a966429ba5fa46124daafa6b2
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-05-04 04:39:55 +0500

    Routes added in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Books from './components/Books';
+import Categories from './components/Categories';
 
 function App() {
   return (
 <div>
-  React App
+  <Navbar />
+  <Routes>
+    <Route exact path="/" element={<Books />} />
+    <Route exact path="categories" element={<Categories />} />
+  </Routes>
+
 </div>
 
   );
