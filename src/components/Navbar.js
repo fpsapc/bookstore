590ad3b27750125bfacf55ba141c8a8057fc6494
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-05-04 05:36:03 +0500

    Add links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <nav>
+    <div>
+      <h2>Bookstore CMS</h2>
+      <ul>
+        <li><Link to="/">Books</Link></li>
+        <li><Link to="categories">Categories</Link></li>
+      </ul>
+    </div>
+  </nav>
 );
 
 export default Navbar;
