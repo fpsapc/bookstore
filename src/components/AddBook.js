63114c973977ab548e6a99703a80db3eaf3efaed
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-05-04 15:15:21 +0500

    Add form for new book

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 const AddBook = () => (
   <div>
-    Add Book
+    <h3>ADD NEW BOOK</h3>
+    <form>
+      <input type="text" placeholder="Book title" />
+      <select key="author">
+        <option value="Authors">All authors</option>
+      </select>
+      <button type="button">Add Book</button>
+    </form>
+
   </div>
 );
 
