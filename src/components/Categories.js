49274ca2521dafa261fd02279296941bf5bafbc9
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-05-04 05:14:13 +0500

    Fix linter errors

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from 'react';
 
-const Categories = () => {
-  return (
-    <div>
-      Categories
-    </div>
-  )
-}
+const Categories = () => (
+  <div>
+    Categories
+  </div>
+);
 
-export default Categories
+export default Categories;
