0ab5ed906478dccfdc6ec58ca126abf21153b3cb
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-05-04 18:42:05 +0500

    Use props to get data from Books

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,28 +1,45 @@
 import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import Proptypes from 'prop-types';
 
-const BookCard = () => (
-  <div>
+const BookCard = ({ book }) => {
+  const renderBooks = book.map((book) => (
+    <>
+      <div className="bookContainer">
+        <div>
+          <p>{ book.category }</p>
+          <p>{ book.title }</p>
+          <p>{ book.author }</p>
+          <ul>
+            <li><button type="button">Comments</button></li>
+            <li><button type="button">Remove</button></li>
+            <li><button type="button">Edit</button></li>
+          </ul>
+        </div>
+        <div>
+          <div>Chart</div>
+          <p>{ book.progres }</p>
+        </div>
+        <div>
+          <p>status</p>
+          <p>{ book.currentChapter }</p>
+          <p>{ book.chapterNumber }</p>
+          <button type="button">Update Progress</button>
+        </div>
+      </div>
+    </>
+  ));
+  return (
     <div>
-      <p>category</p>
-      <p>title</p>
-      <p>author</p>
-      <ul>
-        <li><button type="button">Comments</button></li>
-        <li><button type="button">Remove</button></li>
-        <li><button type="button">Edit</button></li>
-      </ul>
+      {' '}
+      { renderBooks }
+      {' '}
     </div>
-    <div>
-      <div>chart</div>
-      <p>percentage</p>
-    </div>
-    <div>
-      <p>status</p>
-      <p>currentChapter</p>
-      <p>chapterNumber</p>
-      <button type="button">Update Progress</button>
-    </div>
-  </div>
-);
+  );
+};
+
+BookCard.propTypes = {
+  book: Proptypes.arrayOf(Proptypes.shape({})).isRequired,
+};
 
 export default BookCard;
