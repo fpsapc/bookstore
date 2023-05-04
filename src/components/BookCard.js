6b72f9b7b255af5ec2fe7a19efb7925bbe1d832a
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-05-04 15:33:42 +0500

    Create BookCard Structure

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -2,7 +2,26 @@ import React from 'react';
 
 const BookCard = () => (
   <div>
-    Book Card
+    <div>
+      <p>category</p>
+      <p>title</p>
+      <p>author</p>
+      <ul>
+        <li><button type="button">Comments</button></li>
+        <li><button type="button">Remove</button></li>
+        <li><button type="button">Edit</button></li>
+      </ul>
+    </div>
+    <div>
+      <div>chart</div>
+      <p>percentage</p>
+    </div>
+    <div>
+      <p>status</p>
+      <p>currentChapter</p>
+      <p>chapterNumber</p>
+      <button type="button">Update Progress</button>
+    </div>
   </div>
 );
 
